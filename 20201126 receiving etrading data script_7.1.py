84d915df115d9b429fe8f9516c9add279d2a2b9d
Kang <branden.c.w.kang@gmail.com>
2020-11-28 15:58:23 +0900

    upload file

--- 20201126 receiving etrading data script_7.1.py
+++ 20201126 receiving etrading data script_7.1.py
@@ -1,5 +1,4 @@
 #-*-coding: utf-8 -*-
-
 import win32com.client
 import pythoncom
 import time
@@ -7,7 +6,8 @@ import pandas as pd
 import sqlite3
 from datetime import datetime
 
-# 로그인 역할을 수행하는 클래스 생성777
+
+# 로그인 역할을 수행하는 클래스 생성
 class XSession:
     def __init__(self):
         self.login_state = 0
@@ -54,6 +54,8 @@ class XSession:
         XSession = win32com.client.DispatchWithEvents("XA_Session.XASession", cls)  # 서버에 클래스를 요청해서 받아오는 부분 cls 고정 ?????. 결과값은 클래스고, 서버에 3개중에 하나를 요청하는데 쓰이는 메써드
         return XSession            ## XSession 이라는 인스턴스로 ..
 
+
+
 class XQuery_t2105:
     category_code = ""  #왜 빈칸으로 변수를 만드는 가?  왜 이 클래스에 만들어서 다른 클래스에서도 쓰는가???????????????????????????????????????????????????????????????????
     option_code = ""  ## __init__ 에 들어 가야 할 수도
@@ -68,27 +70,20 @@ class XQuery_t2105:
 
     def OnReceiveData(self, tr_code):
         self.is_data_received = True
-        # 사용 항목
+
         price = self.GetFieldData("t2105OutBlock", "price", 0)
         offerho1 = self.GetFieldData("t2105OutBlock", "offerho1", 0)
         bidho1 = self.GetFieldData("t2105OutBlock", "bidho1", 0)
         hotime = self.GetFieldData("t2105OutBlock", "time", 0)
 
         # 비사용 항목
-        # name = self.GetFieldData("t2105OutBlock", "hname", 0)
-        # volume = self.GetFieldData("t2105OutBlock","volume",0)
-        # offerrem1 = self.GetFieldData("t2105OutBlock", "offerrem1", 0)
-        # bidrem1 = self.GetFieldData("t2105OutBlock", "bidrem1", 0)
-        # dcnt1 = self.GetFieldData("t2105OutBlock", "dcnt1", 0)
-        # scnt1 = self.GetFieldData("t2105OutBlock", "scnt1", 0)
+        # hname, volumne, offerrem1, bidrem1, dcnt1, scnt1
 
         print("TR code는 {0}이며, 옵션 코드는 {1}입니다.".format(tr_code,self.option_code))
         print("종목은 {0},\n현재가는 {1},\n 거래가, {2},\n 매도호가1은 {3},\n 매수호가1은  {4}\n".format(name, hotime, price, offerho1, bidho1))
         return_real_items.append([self.option_code, hotime, price, offerho1, bidho1])
 
         status_bundle[self.option_code] =  {"curBidho":bidho1,"curOfferho":offerho1,"preBidho":bidho1,"preOfferho":offerho1,"price":price}
-
-
 
     def request(self):
         option_code = self.option_code
@@ -106,23 +101,23 @@ class XQuery_t2105:
         return xq_t2105
 
 
+
 class XReal_OC0_:
     def __init__(self):
         super().__init__()
         self.count = 0
 
-    def set_data(self,category_code,option_code): ## from 384
-        self.category_code = category_code        ##
+    def set_data(self,category_code,option_code):
+        self.category_code = category_code
         self.option_code = option_code
-                                            # 메써드는 반드시 불러야 실행이 된다,,안불러도 실행 되는 것이 아니고 서버에서 메써드 실행 시킨것
-    def OnReceiveRealData(self, tr_code):  # event handler  ## 이베스트에서 신호를 줘서 실행 된다/ 그쪽에서 코드를 실행했다  겟인스턴스는 그쪽 것을 받아 온것인데...
+
+    def OnReceiveRealData(self, tr_code):
         """
         이베스트 서버에서 ReceiveRealData 이벤트 받으면 실행되는 event handler
         """
 
         self.count += 1
 
-        # 사용 항목
         optcode = self.GetFieldData("OutBlock","optcode")
         chetime = self.GetFieldData("OutBlock", "chetime") # 체결시간
         price = self.GetFieldData("OutBlock", "price") # 현가
@@ -130,20 +125,14 @@ class XReal_OC0_:
         bidho1 = self.GetFieldData("OutBlock", "bidho1") # 매수 1호가(구입)
 
         # 비사용 항목
-        # mdvolume = self.GetFieldData("OutBlock", "mdvolume") # 매도누적체결량
-        # mdchecnt = self.GetFieldData("OutBlock", "mdchecnt") # 매도누적체결건수
-        # msvolume = self.GetFieldData("OutBlock", "msvolume")  # 매수누적체결량
-        # mschecnt = self.GetFieldData("OutBlock", "mschecnt") #매수누적체결건수
-        # open = self.GetFieldData("OutBlock", "open") #시가
-        # high = self.GetFieldData("OutBlock", "high") #고가
-        # low = self.GetFieldData("OutBlock", "low") #저가
-
+        # mdvolume/ 매도누적체결량, mdchecnt/ 매도누적체결건수, msvolume/ 매수누적체결량, mschecnt/ 매수누적체결건수
+        # open/ 시가, high/ 고가, low/ 저가
 
         print("아래 데이터를 수신했습니다.")
         print(optcode,chetime,price,offerho1,bidho1)
         print("\n")
 
-        if status_bundle
+        if optcode in status_bundle.keys():
             status_bundle[optcode]["preOfferho"] = status_bundle[optcode]["curOfferho"]
             status_bundle[optcode]["preBidho"] = status_bundle[optcode]["curBidho"]
 
@@ -155,33 +144,28 @@ class XReal_OC0_:
             print("일치하는 항목이 없습니다.")
             print("\n")
 
+        # 문제가 생긴 부분!!!!!!!!!
+        record_of_each_hoprice[option_code]['bidho'].append(status_bundle[optcode]["curBidho"])
+        record_of_each_hoprice[option_code]['offerho'].append(status_bundle[optcode]["curOfferho"])
+        record_of_each_hoprice[option_code]['count'] += 1
 
-        option_A_bidho_items.append(statesOfOptionA["curBidho"])
-        option_A_offerho_items.append(statesOfOptionA["curOfferho"])
-        option_B_bidho_items.append(statesOfOptionB["curBidho"])
-        option_B_offerho_items.append(statesOfOptionB["curOfferho"])
+
         global isOver150
-        global option_A_offer_state
-        global option_B_bid_state
-        global option_A_bid_state
-        global option_B_offer_state
 
         if isOver150 == False:
-            if len(option_A_bidho_items) > 150:
+            all_over_150_boolean = [1 for idx in record_of_each_hoprice.keys() if record_of_each_hoprice[idx]['count'] < 150]
+            if all_over_150_boolean == True:
                 isOver150 = True
             return_real_items.append([-9999,-9999,chetime,statesOfOptionA["price"],statesOfOptionA["curOfferho"],statesOfOptionA["curBidho"],statesOfOptionB["price"],statesOfOptionB["curOfferho"],statesOfOptionB["curBidho"]])
 
         elif isOver150 == True:
             print("150 이상 돌입")
-            option_A_bidho_items.pop(0)
-            option_A_offerho_items.pop(0)
-            option_B_bidho_items.pop(0)
-            option_B_offerho_items.pop(0)
+            record_of_each_hoprice[option_code]['bidho'].pop(0)
+            record_of_each_hoprice[option_code]['offerho'].pop(0)
 
-            optionA_bidhoAverage = sum(option_A_bidho_items) / len(option_A_bidho_items) # a 매수호가 평균
-            optionA_offerhoAverage = sum(option_A_offerho_items) / len(option_A_offerho_items) # a 매도호가 평균
-            optionB_bidhoAverage = sum(option_B_bidho_items) / len(option_B_bidho_items) # b 매수호가 평균
-            optionB_offerhoAverage = sum(option_B_offerho_items) / len(option_B_offerho_items) # b 매도호가 평균
+            record_of_each_hoprice[option_code]['offerho_average'] = sum(record_of_each_hoprice[option_code]['offerho']) / len(record_of_each_hoprice[option_code]['offerho'])
+            record_of_each_hoprice[option_code]['bidho_average'] = sum(record_of_each_hoprice[option_code]['bidho']) / len(record_of_each_hoprice[option_code]['bidho'])
+
 
 
         # print(statesOfOptionA[0],statesOfOptionB[3]) # A옵션 매도 B 옵션 매수 호가  1.00   0.49
@@ -194,42 +178,48 @@ class XReal_OC0_:
             difference =(statesOfOptionB["curBidho"]*2)-statesOfOptionA["curOfferho"]
             differList2.append(difference)
 
-            # A가 더 비싸다고 가정
-            print("H1_L2",Bid_A_Offer_B) # 비싼것 매도호에 사고  싼것 두개 매수호 산다  비용으로 계산한것  1.00-0.49*2 =0.02 비용 발생
-            print("L2_H1",Bid_B_Offer_A) # 싼것 두개 사고 비싼것 하나 판다                             0.5*2 -0.99 = 0.01 비용 발생
-            print("\n Deviation 값입니다.",deviation) # 대부분 0.04가 정상인데,,,
-            print("\n Difference 값입니다.", difference)
+            append_value = []
 
-            return_real_items.append([deviation,difference,chetime,statesOfOptionA["price"],statesOfOptionA["curOfferho"],statesOfOptionA["curBidho"],statesOfOptionB["price"],statesOfOptionB["curOfferho"],statesOfOptionB["curBidho"]])
+            append_value.append(chetime)
 
-            if differList[-1] >= 0.03 and differList[-2] < 0.03:   #
-                if (option_A_offer_state == False) and (option_B_bid_state == False):
-                    print(first_option_code[0],"1 개 매도(판매) 주문완료")
-                    CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[0],매매구분="1",선물옵션호가유형코드="00",주문가격=statesOfOptionA['curOfferho'],주문수량='1') #
-                    print(first_option_code[1],"2 개 매수(구매) 주문완료")
-                    CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[1],매매구분="2",선물옵션호가유형코드="00",주문가격=statesOfOptionB['curBidho'],주문수량='2') #
-                    #@1 매도(판매) @2 매수(구입)
-                    global upperCaseCount
-                    upperCaseCount += 1
+            for indicator_name in indicator_names:
+                append_value.append(statistics_bundle[indicator_name])
+
+            for option_code in option_codes:
+                append_value.append(status_bundle[option_code]['price'])
+                append_value.append(status_bundle[option_code]['curOfferho'])
+                append_value.append(status_bundle[option_code]['curBidho'])
 
 
-                    option_A_offer_state = True
-                    option_B_bid_state = True
 
-            elif differList[-1] <= -0.03 and differList[-2] > -0.03:
-                if (option_A_bid_state == False) and (option_B_offer_state == False):
-                    print(first_option_code[0],"2 개 매도(판매) 주문완료")
-                    CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[1],매매구분="1",선물옵션호가유형코드="00",주문가격=statesOfOptionB['curOfferho'],주문수량='2')
-                    print(first_option_code[1],"1 개 매수(구매) 주문완료")
-                    CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[0],매매구분="2",선물옵션호가유형코드="00",주문가격=statesOfOptionA['curBidho'],주문수량='1')
-                    global lowerCaseCount
-                    lowerCaseCount += 1
-
-                    option_A_bid_state = True
-                    option_B_offer_state = True
-
-            else:
-                print("주문없이 종료시켰습니다.")
+            # if differList[-1] >= 0.03 and differList[-2] < 0.03:   #
+            #     if (option_A_offer_state == False) and (option_B_bid_state == False):
+            #         print(first_option_code[0],"1 개 매도(판매) 주문완료")
+            #         CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[0],매매구분="1",선물옵션호가유형코드="00",주문가격=statesOfOptionA['curOfferho'],주문수량='1') #
+            #         print(first_option_code[1],"2 개 매수(구매) 주문완료")
+            #         CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[1],매매구분="2",선물옵션호가유형코드="00",주문가격=statesOfOptionB['curBidho'],주문수량='2') #
+            #         #@1 매도(판매) @2 매수(구입)
+            #         global upperCaseCount
+            #         upperCaseCount += 1
+            #
+            #
+            #         option_A_offer_state = True
+            #         option_B_bid_state = True
+            #
+            # elif differList[-1] <= -0.03 and differList[-2] > -0.03:
+            #     if (option_A_bid_state == False) and (option_B_offer_state == False):
+            #         print(first_option_code[0],"2 개 매도(판매) 주문완료")
+            #         CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[1],매매구분="1",선물옵션호가유형코드="00",주문가격=statesOfOptionB['curOfferho'],주문수량='2')
+            #         print(first_option_code[1],"1 개 매수(구매) 주문완료")
+            #         CFOAT00100(계좌번호=str(int(id_info[1])),비밀번호=id_info[4],선물옵션종목번호=option_codes[0],매매구분="2",선물옵션호가유형코드="00",주문가격=statesOfOptionA['curBidho'],주문수량='1')
+            #         global lowerCaseCount
+            #         lowerCaseCount += 1
+            #
+            #         option_A_bid_state = True
+            #         option_B_offer_state = True
+            #
+            # else:
+            #     print("주문없이 종료시켰습니다.")
             print("\n")
 
     def start(self):
@@ -240,15 +230,11 @@ class XReal_OC0_:
         self.SetFieldData("InBlock", "optcode",  self.option_code)
         self.AdviseRealData()   # 실시간데이터 요청
 
-
-
-    def add_item(self, stockcode):                          # 이것이 왜 필요하지??????????????????????????????????
-        # 실시간데이터 요청 종목 추가
-        self.SetFieldData("InBlock", "optcode", stockcode)  ## stock 코드 맞는가? 옵션코드 아닌가 확인    365에서 옴  ???????????????????????????????????
-        self.AdviseRealData()                               ## 실시간 리얼데이타를 등록한다
+    def add_item(self, stockcode):
+        self.SetFieldData("InBlock", "optcode", stockcode)
+        self.AdviseRealData()
 
     def remove_item(self, stockcode):
-        # stockcode 종목만 실시간데이터 요청 취소
         self.UnadviseRealDataWithKey(stockcode)
 
     def end(self):
@@ -261,7 +247,6 @@ class XReal_OC0_:
 
 
 
-
 class XAQueryEvents:
     상태 = False
 
@@ -271,9 +256,6 @@ class XAQueryEvents:
 
     def OnReceiveMessage(self, systemError, messageCode, message):
         print("OnReceiveMessage : ", systemError, messageCode, message)
-
-
-
 
 
 
@@ -323,28 +305,23 @@ if __name__ == "__main__":
 
         # Password 파일에서 원하는 계좌정보 선택
         info_df = pd.read_csv("./secret/passwords.csv", encoding='cp949')
-        print("\n",info_df)
-        info_Num = input("\n※ 사용하실 계좌를 선택해주십시오:")
+        print("\n",info_df,"\n")
+        info_Num = input("※ 사용하실 계좌를 선택해주십시오:\n")
         id_info = info_df.loc[int(info_Num)].tolist()
         print(id_info,"\n")
 
         # code_list 파일에서 원하는 옵션 코드 불러오기
         code_list_df = pd.read_csv("./secret/code_list.csv", encoding='cp949')
-        print(code_list_df,"\n") # 이부분 주석처리 가능---code_list_df는 데이타프레임
-        code_Num = input("\n※ 사용하실 코드를 선택해주십시오:")
-        option_codes = code_list_df.loc[int(code_Num)].tolist() # 리스트로 변경된 부분
+        print(code_list_df,"\n")
+        code_Num = input("※ 사용하실 코드를 선택해주십시오:\n")
+        option_codes = code_list_df.loc[int(code_Num)].tolist()
         print("※ 다운로드 받을 옵션 코드는 다음과 같습니다.(벌크 다운로드용 기능입니다.)")
-        print(option_codes,"\n")                          # ['201QA327', '201QA332']
+        print(option_codes,"\n")
 
         # 몇시 몇분까지 데이터를 받아올 지 입력
         print("※ 언제까지 데이터를 수신할까요? \n 입력 예제) 3시 15분 - 1515")
         due_time = input()
         print("※",due_time,"까지 데이터를 수신합니다.\n")
-
-
-        ## 데이터를 받을 코드 단일(String) 생성
-        first_option_code = option_codes[0] # 삭제예정
-        print(option_codes,"\n")
 
         # "0.03 이상 혹은 -0.03 이하가 되는 횟수"를 저장할 변수를 생성 (Integer)
         lowerCaseCount = 0
@@ -352,15 +329,17 @@ if __name__ == "__main__":
 
         status_bundle = {}
         statistics_bundle = {}
-        dictionary_of_code = {}
-        ordinal_numbers = ['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth','thirteenth','fourteenth','fifteenth']
+        record_of_each_hoprice = {}
+        indicator_names = ['difference_log','deviation_log']
 
         for idx,option_code in enumerate(option_codes):
             dictionary_of_code[option_code] = ordinal_numbers[idx]
             status_bundle[option_code] =  {"curBidho":-9999.0,"curOfferho":-9999.0,"preBidho":-9999.0,"preOfferho":-9999.0,"price":-9999.0}
+            record_of_each_hoprice[option_code] = { 'bidho': [] , 'offerho': [], 'count': 0, 'bid_status' = False, 'offer_status' = False, 'bidho_average' = 0, 'offerho_average' = 0 }
 
-        for log_title in ['difference_log','deviation_log']:
-            statistics_bundle[log_title] = {option_code:[-9999,-9999] for option_code in option_codes}
+
+        for indicator_name in indicator_names:
+            statistics_bundle[indicator_name] = {option_code:[-9999,-9999] for option_code in option_codes}
 
 
 
@@ -369,6 +348,7 @@ if __name__ == "__main__":
         differList2 = [-9999,-9999]
         # 리얼 데이터를 저장할 묶음(List)
         return_real_items = []
+
 
 
 
@@ -433,6 +413,8 @@ if __name__ == "__main__":
                 print("lowerCaseCount: ",lowerCaseCount)
                 time.sleep(10)
                 print("---- end -----")
+
+
 
 
                 # if old_count < xreal.count:
